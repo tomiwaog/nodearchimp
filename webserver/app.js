99c62d72943551f40fc5531704aa6d211eac388d
Tomiwa Ogunbamowo <contact@tomiwa.io>
2018-12-16 12:28:40 +0000

    Node webserver with requests options- ie HTML, JSON and else

--- webserver/app.js
+++ webserver/app.js
@@ -1,13 +1,26 @@
 var http = require('http');
 var fs = require('fs');
 
-http.createServer(function(req, res){
+http.createServer(function (req, res) {
+    if (req.url === '/app') {
+        //Outputting JSON
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        var obj = {
+            firstname: 'Tomiwa',
+            lastname: 'Ogunbamowo'
+        }
+        res.end(JSON.stringify(obj));
+    }
+    else if (req.url === '/') {
+        res.writeHead(200, { 'Content-Type': 'text/html' });// Outputting HTML
+        fs.createReadStream(__dirname + '/index.html').pipe(res); //Using Asynchronous stream - Piping
+    }
+    else {
+        res.writeHead(200, { 'Content-Type': 'text/html' });// Outputting HTML
+        var html = fs.readFileSync(__dirname + '/index.html', 'utf8') //Store content of file to html var
+        html = html.replace('{Message}', '404 - Page not found on');
+        res.end(html);
+    }
 
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    //var html = fs.readFileSync(__dirname+'/index.html', 'utf8');
-    // html = html.replace('{Message}', "OGA MI New Message replacement");
-    // res.end(html);
 
-    //Using Asynchronous stream
-    fs.createReadStream(__dirname+'/index.html').pipe(res)
 }).listen(1337, '127.0.0.1');
