f943d2b96a23e700b9641a5a361ec14264c32d07
Tomiwa Ogunbamowo <contact@tomiwa.io>
2018-12-16 13:03:13 +0000

    Added protocol header for 404 pages

--- webserver/app.js
+++ webserver/app.js
@@ -15,12 +15,12 @@ http.createServer(function (req, res) {
         res.writeHead(200, { 'Content-Type': 'text/html' });// Outputting HTML
         fs.createReadStream(__dirname + '/index.html').pipe(res); //Using Asynchronous stream - Piping
     }
+    else {
+        res.writeHead(404, { 'Content-Type': 'text/html' });// Outputting HTML
+        var html = fs.readFileSync(__dirname + '/index.html', 'utf8') //Store content of file to html var
+        html = html.replace('{Message}', `404 - Page not found on '${req.url}'`);
+        res.end(html);
 
-    res.writeHead(404, { 'Content-Type': 'text/html' });// Outputting HTML
-    var html = fs.readFileSync(__dirname + '/index.html', 'utf8') //Store content of file to html var
-    html = html.replace('{Message}', `404 - Page not found on '${req.url}'`);
-    res.end(html);
-
-
+    }
 
 }).listen(1337, '127.0.0.1');
