915739861cb0577ccde11a791dce74d0ef9283af
Tomiwa Ogunbamowo <contact@tomiwa.io>
2018-12-17 23:58:16 +0000

    Basic Express Server implementation

---
+++ expressserver/app.js
@@ -0,0 +1,11 @@
+var express = require('express/lib/express');
+var app = express();
+
+
+var port = process.env.PORT || 3000; //If ENV port is set use it, else 3000
+
+app.get('/', function(req, res){
+    res.send('<html><head><title>Toms Express Serevr</title></head><body><h1>Header 1</h1>Hello World on Express Server</body></html>');
+})
+
+app.listen(port);
