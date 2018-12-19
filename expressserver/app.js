66ad2e64f65862d88aae6e4d6f153365e850f1e0
Tomiwa Ogunbamowo <contact@tomiwa.io>
2018-12-19 22:02:58 +0000

    Node middleware, static files and routing

--- expressserver/app.js
+++ expressserver/app.js
@@ -5,19 +5,32 @@ var fs = require('fs');
 
 var port = process.env.PORT || 3000; //If ENV port is set use it, else 3000
 
-app.get('/', function(req, res){
-    res.send('<html><head><title>Toms Express Server</title></head><body><h1>Header 1</h1>Hello World on Express Server</body></html>');
+app.get('/', function (req, res) {
+    res.send('<html><head><title>Toms Express Server</title><link href=assets/style.css type=text/css rel=stylesheet></head><body><h1>Header 1</h1>Hello World on Express Server</body></html>');
 })
 
-app.get('/txt', function(req, res){
+app.get('/txt', function (req, res) {
     res.send("Just responding text");
 })
 
-app.get('/api', function(req, res){
-    res.json({name: 'FirstName Here', lastName: 'Last Name Here', empID: 'EMP232'})
+app.get('/api', function (req, res) {
+    res.json({ name: 'FirstName Here', lastName: 'Last Name Here', empID: 'EMP232' })
 })
 
-app.get('/home', function(req, res){
+//Maps route to a directory | Middleware between request and response
+app.use('/assets', express.static(__dirname+'/public') );
+
+app.get('/mid/:param', (req, res) => {
+    var param = req.params.param;
+    if (param === 'tom') {
+        res.send(`Tom. This is a middleware with param '${param}'`);
+    }
+    else {
+        res.send('Other Param Specified!');
+    }
+});
+
+app.get('/home', function (req, res) {
     fs.createReadStream(__dirname + '/index.html').pipe(res);
 })
 app.listen(port);
